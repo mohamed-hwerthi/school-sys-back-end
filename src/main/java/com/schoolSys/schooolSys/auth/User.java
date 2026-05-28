f224cb7de937bfe5817eb2db023279dd5e3dcec6
aloui_houssem <houssem.aloui@enicar.ucar.tn>
2026-05-28 20:22:44 +0200

    Rename database table from 'users' to 'users_school'

--- src/main/java/com/schoolSys/schooolSys/auth/User.java
+++ src/main/java/com/schoolSys/schooolSys/auth/User.java
@@ -9,7 +9,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users", schema = "public")
+@Table(name = "users_school", schema = "public")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
